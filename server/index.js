63c3c3011be2e966e44df70a66ce3759a5602d07
Aakash S <aakash@Aakashs-MacBook-Air.local>
2024-04-20 10:50:58 +0530

    Feat: Created server with necessary routers

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const env = require("dotenv")
 env.config()
 const express = require("express");
+const cors = require("cors")
+const quizRouter = require("./router/quiz.router")
 app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1",quizRouter)
 
 app.get("/", (req, res) => {
   res.status(200).json({ Message: "Hello World" });
 });
 
-app.listen(process.env.PORT, (req, res) => {
+app.listen(process.env.PORT, () => {
   console.log(`Server listening on PORT ${process.env.PORT}`);
 });
--- server/router/quiz.router.js
+++ server/router/quiz.router.js
@@ -1,1 +1,39 @@
-// Router Configuration will be done here
+const express = require("express");
+const router = express.Router();
+const data = require("../data/data");
+const answerData = require("../demo");
+
+router.get("/questions", async (req, res) => {
+  try {
+    if (!data) {
+      return res.status(404).json({ error: "Question not found" });
+    }
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching question:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.post("/questions/:id",async(req,res)=>{
+  try{
+    const id= Number(req.params.id)
+    const aid = Number(req.body.aid);
+
+    const filterData = data.filter((data)=>{
+      if(id===data._id) return data;
+    })
+    console.log(filterData[0].correctAnswer===aid);
+    if(filterData[0].correctAnswer===aid) {
+      return res.status(200).json({data:true})
+    }
+    res.status(200).json({data:false,message:filterData[0].feedback})
+  }
+  catch(err){
+    console.log(err);
+    res.status(500).json({error:"Internal server error"});
+  }
+})
+
+
+module.exports = router;
