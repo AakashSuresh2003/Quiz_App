8d65afd25cc7b00202d0a48428eaeb47f684a773
Aakash S <aakash@Aakashs-MacBook-Air.local>
2024-04-20 18:10:55 +0530

    Fix : reload bug fixed

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const env = require("dotenv")
-env.config()
+const env = require("dotenv");
+env.config();
 const express = require("express");
-const cors = require("cors")
-const path = require("path")
+const cors = require("cors");
 const quizRouter = require("./router/quiz.router");
 const connectDb = require("./database/db.config");
 
@@ -11,15 +10,14 @@ app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(__dirname+"/public"))
-app.get("/",(req,res)=>{
-  res.render("index.html")
-})
-
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  res.render("index.html");
+});
 
 connectDb();
 
-app.use("/api/v1",quizRouter)
+app.use("/api/v1", quizRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({ Message: "Hello World" });
